5093ea993c58cc3b24c40eedc9830fa65fc02c27
Raghav Mittal <justcrack1206@gmail.com>
2023-04-29 13:40:17 +0530

    changes:

--- src/main/java/com/darzee/shankh/request/innerObjects/UpdateOrderAmountDetails.java
+++ src/main/java/com/darzee/shankh/request/innerObjects/UpdateOrderAmountDetails.java
@@ -1,8 +1,11 @@
 package com.darzee.shankh.request.innerObjects;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Data;
 
 @Data
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class UpdateOrderAmountDetails {
     private Double totalOrderAmount;
     private Double advanceOrderAmount;
