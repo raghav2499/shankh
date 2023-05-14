e170367c33dc6ac19d6aa68aff6c6641cc4d3a46
Raghav Mittal <justcrack1206@gmail.com>
2023-05-14 17:16:06 +0530

    Payment mode handling changed

--- src/main/java/com/darzee/shankh/request/RecievePaymentRequest.java
+++ src/main/java/com/darzee/shankh/request/RecievePaymentRequest.java
@@ -1,6 +1,5 @@
 package com.darzee.shankh.request;
 
-import com.darzee.shankh.enums.PaymentMode;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Data;
@@ -13,7 +12,7 @@ import java.time.LocalDate;
 public class RecievePaymentRequest {
 
     @NotNull(message = "payment_mode is a required param")
-    private PaymentMode paymentMode;
+    private Integer paymentMode;
     @NotNull(message = "payment_date is a required param")
     private LocalDate paymentDate;
     @NotNull(message = "amount is a required param")
