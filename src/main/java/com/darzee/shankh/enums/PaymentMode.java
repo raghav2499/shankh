08dfdaeb0103a57d7c56daa906ea1e22de61f530
Raghav Mittal <justcrack1206@gmail.com>
2023-05-14 18:17:06 +0530

    Payment modes added

--- src/main/java/com/darzee/shankh/enums/PaymentMode.java
+++ src/main/java/com/darzee/shankh/enums/PaymentMode.java
@@ -4,7 +4,12 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 
 @JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum PaymentMode {
-    CASH("cash", 1);
+    CASH("cash", 1),
+    BANK_TRANSFER("Bank Transfer", 2),
+    CARD_SWIPE("Card Swipe", 3),
+    UPI("UPI", 4),
+    OTHER("Other", 5);
+
 
     private String mode;
     private Integer ordinal;
