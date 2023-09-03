3ac28cbfaac1c1589f098788b3786b08966d1573
Raghav Mittal <justcrack1206@gmail.com>
2023-09-03 14:28:15 +0530

    changes

--- src/main/java/com/darzee/shankh/config/WebSecurityConfig.java
+++ src/main/java/com/darzee/shankh/config/WebSecurityConfig.java
@@ -43,7 +43,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests().antMatchers("/tailor/signup", "/tailor/login", "/storage/",
-                        "/storage/uploadFile", "/storage/downloadFile", "/actuator/prometheus").permitAll()
+                        "/storage/uploadFile", "/storage/downloadFile", "/actuator/prometheus", "/portfolio/",
+                        "/portfolio/portfolio_outfit", "/outfit/sub_outfit", "/outfit/").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .exceptionHandling().authenticationEntryPoint(authenticationEntryPoint)
