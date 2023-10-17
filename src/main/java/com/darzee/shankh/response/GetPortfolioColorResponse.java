936c0f823c05d73dc3bff938f425f3de86f6f9de
Raghav Mittal <justcrack1206@gmail.com>
2023-10-17 21:54:34 +0530

    minor changes

--- src/main/java/com/darzee/shankh/response/GetPortfolioColorResponse.java
+++ src/main/java/com/darzee/shankh/response/GetPortfolioColorResponse.java
@@ -1,5 +1,7 @@
 package com.darzee.shankh.response;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,6 +11,7 @@ import java.util.List;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class GetPortfolioColorResponse {
     private List<PortfolioColorDetail> colorDetail;
 }
--- src/main/java/com/darzee/shankh/response/PortfolioColorDetail.java
+++ src/main/java/com/darzee/shankh/response/PortfolioColorDetail.java
@@ -1,10 +1,12 @@
 package com.darzee.shankh.response;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @NoArgsConstructor
+@Data
 public class PortfolioColorDetail {
     private Integer ordinal;
     private String name;
