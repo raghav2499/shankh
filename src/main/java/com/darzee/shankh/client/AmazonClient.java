ff9a03fe2b9e9f627acc77e99f369d77124b2507
Ashutosh Jha <98846260+ashdude1401@users.noreply.github.com>
2024-04-21 16:23:22 +0530

    tiny url for pdf added

--- src/main/java/com/darzee/shankh/client/AmazonClient.java
+++ src/main/java/com/darzee/shankh/client/AmazonClient.java
@@ -11,10 +11,17 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
+
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 public class AmazonClient {
@@ -131,7 +138,17 @@ public class AmazonClient {
 
     private String generateShortLivedUrl(String bucketName, String fileName) {
         String region = S3Bucket.getBucketRegionMap().get(bucketName);
-        String shortLivedUrl = "https://s3." + region + ".amazonaws.com/" + bucketName + "/" + fileName;
-        return shortLivedUrl;
+        String longUrl = "https://s3." + region + ".amazonaws.com/" + bucketName + "/" + fileName;
+    
+        try {
+            URL url = new URL("http://tinyurl.com/api-create.php?url=" + URLEncoder.encode(longUrl, "UTF-8"));
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String shortUrl = rd.readLine();
+            return shortUrl;
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to generate short URL", e);
+        }
     }
 }
