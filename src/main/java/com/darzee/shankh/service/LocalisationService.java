14519c71696e2f97c0b448d1401afe8a976a5262
ashdude1401 <ashdudecool1401@gmail.com>
2024-05-22 21:23:39 +0530

    feat: Translate suit names in multiple languages

--- src/main/java/com/darzee/shankh/service/LocalisationService.java
+++ src/main/java/com/darzee/shankh/service/LocalisationService.java
@@ -1,8 +1,24 @@
 package com.darzee.shankh.service;
 
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LocalisationService {
-    
+        private final MessageSource messageSource;
+
+    public LocalisationService(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+
+    public String translate(String englishText) {
+        String key = englishText.toLowerCase().replace(" ", ".");
+        // Locale locale = LocaleContextHolder.getLocale();
+        //hindi locale 
+        Locale locale = new Locale("hi", "IN");
+        return messageSource.getMessage(key, null, locale);
+    }
 }
