a8baa90eec7bd687cd91be0b60ba27cf3ac3a057
ashdude1401 <ashdudecool1401@gmail.com>
2024-05-22 21:22:58 +0530

    chore: Translate stitch option labels and values in GetStitchOptionsResponse

--- src/main/java/com/darzee/shankh/response/GetStitchOptionsResponse.java
+++ src/main/java/com/darzee/shankh/response/GetStitchOptionsResponse.java
@@ -2,6 +2,7 @@ package com.darzee.shankh.response;
 
 import com.darzee.shankh.dao.StitchOptionsDAO;
 import com.darzee.shankh.enums.OutfitSide;
+import com.darzee.shankh.service.LocalisationService;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
@@ -12,10 +13,16 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 @Data
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class GetStitchOptionsResponse {
+    
+    @Autowired
+    LocalisationService localisationService;
+
     private List<GroupedStitchOptionDetails> response;
 
     public GetStitchOptionsResponse(List<StitchOptionsDAO> stitchOptions) {
@@ -23,15 +30,24 @@ public class GetStitchOptionsResponse {
                 stitchOptions.stream().collect(Collectors.groupingBy(StitchOptionsDAO::getOutfitSide));
         List<GroupedStitchOptionDetails> response = new ArrayList<>();
         if(groupedByOutfitSide.containsKey(OutfitSide.TOP)) {
-            List<StitchOptionDetail> stitchOptionDetails = groupedByOutfitSide.get(OutfitSide.TOP).stream().map(stitchOptionsDAO ->
-                    new StitchOptionDetail(stitchOptionsDAO)).collect(Collectors.toList());
-            GroupedStitchOptionDetails topStitchOptionDetails = new GroupedStitchOptionDetails(OutfitSide.TOP.getView(), stitchOptionDetails);
+            List<StitchOptionDetail> stitchOptionDetails = groupedByOutfitSide.get(OutfitSide.TOP).stream().map(stitchOptionsDAO ->{
+                StitchOptionDetail stitchOptionDetail=   new StitchOptionDetail(stitchOptionsDAO);                                  stitchOptionDetail.setLabel(localisationService.translate(stitchOptionDetail.getLabel()));
+                stitchOptionDetail.getOptions().forEach(optionDetail -> optionDetail.setLabel(localisationService.translate(optionDetail.getLabel())));
+                return stitchOptionDetail;
+            } ).collect(Collectors.toList());
+
+            GroupedStitchOptionDetails topStitchOptionDetails = new GroupedStitchOptionDetails(
+               localisationService.translate(OutfitSide.TOP.getView()) , stitchOptionDetails);
             response.add(topStitchOptionDetails);
         }
         if(groupedByOutfitSide.containsKey(OutfitSide.BOTTOM)) {
-            List<StitchOptionDetail> stitchOptionDetails = groupedByOutfitSide.get(OutfitSide.BOTTOM).stream().map(stitchOptionsDAO ->
-                    new StitchOptionDetail(stitchOptionsDAO)).collect(Collectors.toList());
-            GroupedStitchOptionDetails bottomStitchOptionDetails = new GroupedStitchOptionDetails(OutfitSide.BOTTOM.getView(), stitchOptionDetails);
+            List<StitchOptionDetail> stitchOptionDetails = groupedByOutfitSide.get(OutfitSide.BOTTOM).stream().map(stitchOptionsDAO ->{
+                StitchOptionDetail stitchOptionDetail=   new StitchOptionDetail(stitchOptionsDAO);                                  stitchOptionDetail.setLabel(localisationService.translate(stitchOptionDetail.getLabel()));
+                stitchOptionDetail.getOptions().forEach(optionDetail -> optionDetail.setLabel(localisationService.translate(optionDetail.getLabel())));
+                return stitchOptionDetail;
+            } ).collect(Collectors.toList());
+            GroupedStitchOptionDetails bottomStitchOptionDetails = new GroupedStitchOptionDetails( 
+               localisationService.translate(OutfitSide.BOTTOM.getView()), stitchOptionDetails);
             response.add(bottomStitchOptionDetails);
         }
         this.response = response;
