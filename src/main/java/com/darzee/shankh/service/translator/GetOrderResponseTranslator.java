7a027c855387b508fc2152926ba29b21bb485457
ashdude1401 <ashdudecool1401@gmail.com>
2024-06-13 05:26:04 +0530

    Refactor GetOrderResponseTranslator to improve code readability and remove unused dependencies

--- src/main/java/com/darzee/shankh/service/translator/GetOrderResponseTranslator.java
+++ src/main/java/com/darzee/shankh/service/translator/GetOrderResponseTranslator.java
@@ -1,25 +1,19 @@
 package com.darzee.shankh.service.translator;
 
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.darzee.shankh.response.GetOrderResponse;
-import com.darzee.shankh.response.OrderStitchOptionDetail;
 import com.darzee.shankh.service.LocalisationService;
 
 @Service
 public class GetOrderResponseTranslator {
     
     @Autowired
-    private LocalisationService localisationService;
+    private  OrderDetailResponseTranslator orderDetailResponseTranslator;
 
-    @Autowired
-    private OrderStitchOptionsTranslator orderStitchOptionsTranslator;
-
-    @Autowired
-    private MeasurementDetailsTranslator measurementDetailsTranslator;
+    @Autowired LocalisationService localisationService;
     
 
     public String getTranslatedMessage(String message) {
@@ -29,18 +23,8 @@ public class GetOrderResponseTranslator {
     public GetOrderResponse getTranslatedOrderDetailList(GetOrderResponse response) {
         response.setMessage(localisationService.translate(response.getMessage()));
         if(response.getData()!=null){
-            response.getData().forEach(orderDetailResponse -> { 
-                            orderDetailResponse.getOrderItemDetails().forEach(orderItemDetails -> { 
-                                
-                                if(orderItemDetails.getOrderItemStitchOptions()!=null){
-                                    orderItemDetails.getOrderItemStitchOptions().forEach((key, value) -> {
-                                                List<OrderStitchOptionDetail> orderStitchOptionDetails = value;                                           orderStitchOptionsTranslator.translate(orderStitchOptionDetails);
-                                            }
-                                    );
-                                }
-                               if(orderItemDetails.getMeasurementDetails()!=null&&orderItemDetails.getMeasurementDetails().getInnerMeasurementDetails()!=null) {               measurementDetailsTranslator.getTranslatedInnerMeasurementDetailsList(orderItemDetails.getMeasurementDetails().getInnerMeasurementDetails());
-                               }
-                            });
+            response.getData().forEach(orderDetailResponse -> {
+                            orderDetailResponseTranslator.translate(orderDetailResponse);
                         });
         }
         return response;
