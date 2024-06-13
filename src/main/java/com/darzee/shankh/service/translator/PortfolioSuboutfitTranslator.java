3e5ba8890f9ea4ab453f6ce5ca1d5da1a66f5013
ashdude1401 <ashdudecool1401@gmail.com>
2024-06-13 14:08:53 +0530

    Refactor PortfolioSuboutfitTranslator to use @Service annotation

--- src/main/java/com/darzee/shankh/service/translator/PortfolioSuboutfitTranslator.java
+++ src/main/java/com/darzee/shankh/service/translator/PortfolioSuboutfitTranslator.java
@@ -1,10 +1,12 @@
 package com.darzee.shankh.service.translator;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.darzee.shankh.response.SubOutfitTypeDetailResponse;
 import com.darzee.shankh.service.LocalisationService;
 
+@Service
 public class PortfolioSuboutfitTranslator {
 
     @Autowired
