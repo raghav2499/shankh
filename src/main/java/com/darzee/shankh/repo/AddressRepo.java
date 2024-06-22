9b0fcc932bd1632a479c2941ad7ae58a9effc18c
ashdude1401 <ashdudecool1401@gmail.com>
2024-06-22 17:48:40 +0530

    feat: Add AddressRepo interface for accessing address data

---
+++ src/main/java/com/darzee/shankh/repo/AddressRepo.java
@@ -0,0 +1,14 @@
+package com.darzee.shankh.repo;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.darzee.shankh.entity.Address;
+
+
+
+public interface AddressRepo extends JpaRepository<Address, Long> {
+
+    Optional<Address> findByAddressId(Long addressId);
+} 
