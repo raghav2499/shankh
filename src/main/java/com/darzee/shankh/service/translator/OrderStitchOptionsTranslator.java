ca72e2aa43b92379d02e59916990332187a937cf
ashdude1401 <ashdudecool1401@gmail.com>
2024-06-24 01:06:46 +0530

    feat: Update translations for hip circumference in multiple languages change lanugage data type in create, update boutique added top and bottom translation in order stitch option details

--- src/main/java/com/darzee/shankh/service/translator/OrderStitchOptionsTranslator.java
+++ src/main/java/com/darzee/shankh/service/translator/OrderStitchOptionsTranslator.java
@@ -16,26 +16,25 @@ public class OrderStitchOptionsTranslator {
     private LocalisationService localisationService;
 
     public List<OrderStitchOptionDetail> translate(List<OrderStitchOptionDetail> orderStitchOptionDetails) {
-        for(OrderStitchOptionDetail orderStitchOptionDetail : orderStitchOptionDetails) {
+        for (OrderStitchOptionDetail orderStitchOptionDetail : orderStitchOptionDetails) {
             orderStitchOptionDetail.setLabel(localisationService.translate(orderStitchOptionDetail.getLabel()));
             orderStitchOptionDetail.setValue(localisationService.translate(orderStitchOptionDetail.getValue()));
+            orderStitchOptionDetail.setOutfitSide(localisationService.translate(orderStitchOptionDetail.getOutfitSide()));
         }
+
         return orderStitchOptionDetails;
     }
 
-    public GetOrderStitchOptionResponse translateGetOrderStitchOptionResponse(GetOrderStitchOptionResponse  response) {
-        response.getResponse().forEach(
-                groupedOrderStitchOptionDetail1 -> groupedOrderStitchOptionDetail1.getStitchOptions().forEach(
-                        stitchOption-> {
-                stitchOption.setLabel(localisationService.translate(stitchOption.getLabel()));
-                stitchOption.setValue(localisationService.translate(stitchOption.getValue())); 
-                        }
-                ) 
-        );
+    public GetOrderStitchOptionResponse translateGetOrderStitchOptionResponse(GetOrderStitchOptionResponse response) {
+        response.getResponse().forEach(groupedOrderStitchOptionDetail -> {
+            groupedOrderStitchOptionDetail
+                    .setSide(localisationService.translate(groupedOrderStitchOptionDetail.getSide()));
+            translate(groupedOrderStitchOptionDetail.getStitchOptions());
+        });
         return response;
     }
 
-    public List<StitchOptionDetail> translateStichOptionList(List<StitchOptionDetail> stitchOptionDetails ){
+    public List<StitchOptionDetail> translateStichOptionList(List<StitchOptionDetail> stitchOptionDetails) {
 
         stitchOptionDetails.forEach(stitchOption -> {
             if (stitchOption != null) {
@@ -52,4 +51,3 @@ public class OrderStitchOptionsTranslator {
         return stitchOptionDetails;
     }
 }
-
